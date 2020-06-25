1763e164735c4ffbcee48bd884f4a10c663c0524
Daniel-N-Huss <daniel.n.huss@gmail.com>
2020-06-25 02:58:28 +0000

    admin dashboard now shows basic counts of products and categories in a MVC way

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,6 +3,8 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
     @products = @category.products.order(created_at: :desc)
+
+    render 'show', products: @products, categories: @category
   end
 
 end
