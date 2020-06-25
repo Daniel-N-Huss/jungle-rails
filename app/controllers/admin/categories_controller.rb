54027275e70fd9f5d87c5a4467a8f63eb7c51a06
Daniel-N-Huss <daniel.n.huss@gmail.com>
2020-06-25 17:40:24 +0000

    basic route and controller generated

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,21 @@
+class Admin::CategoriesController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+
+  private
+
+  def category_params
+  end
+  
+end
